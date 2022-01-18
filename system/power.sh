2ab3056b95e0c8e29dde17f10b4974ca670a6ad8
Holler János <hollerjanika@gmail.com>
2022-01-19 00:51:34 +0100

    Adding comments to the power script

--- system/power.sh
+++ system/power.sh
@@ -1,13 +1,29 @@
 #! /usr/bin/env bash
 
+#==============================
+# Introduction(s)
+#==============================
+
+# Creator: Holler János
+# First release: 2022-01-16 03:12:14
+# Latest update: 2022-01-16 03:12:24
+# Editor: NVIM
+
+#==============================
+# Process
+#==============================
+
+# Declare: All the possible options
 declare -a options=(
   "Suspend"
   "Reboot"
   "Power OFF"
 )
 
+# Display & also store the choice of the user
 choice=$(printf "%s\n" "${options[@]}" | dmenu -i -l 20 -p "?")
 
+# Possible results:
 case "${choice}" in
   "Suspend")
     systemctl suspend
